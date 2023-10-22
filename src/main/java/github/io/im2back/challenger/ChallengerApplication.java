c679c3758c9d5fb54516d5fb4a2d8f0541434301
Jefferson Richards <jefferson.richards@outlook.com>
2023-10-22 08:51:22 -0300

    arquivos iniciais do projeto

---
+++ src/main/java/github/io/im2back/challenger/ChallengerApplication.java
@@ -0,0 +1,13 @@
+package github.io.im2back.challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerApplication.class, args);
+	}
+
+}
